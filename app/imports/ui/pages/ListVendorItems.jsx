679eb11a9ca268672859aa154383149a7807c9b6
ryanm73 <60198841+ryanm73@users.noreply.github.com>
2020-04-16 11:12:49 -1000

    Update ListVendorItems.jsx

--- app/imports/ui/pages/ListVendorItems.jsx
+++ app/imports/ui/pages/ListVendorItems.jsx
@@ -18,7 +18,7 @@ class ListVendorItems extends React.Component {
   renderPage() {
     return (
         <Container>
-          <Header as="h2" textAlign="center">List Stuff</Header>
+          <Header as="h2" textAlign="center">Ingredients</Header>
           <Table celled>
             <Table.Header>
               <Table.Row>
