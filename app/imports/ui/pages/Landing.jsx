33948c0f4753c4353a84e751f90aa50c136ba9e2
Michael Rogers <mlr2010@hawaii.edu>
2020-04-16 19:24:10 -1000

    Added a simple intro block to the landing page

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,24 +1,26 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid, Image, Header } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
-        <Grid verticalAlign='middle' textAlign='center' container>
-
-          <Grid.Column width={4}>
-            <Image size='small' circular src="/images/meteor-logo.png"/>
-          </Grid.Column>
-
-          <Grid.Column width={8}>
-            <h1>Welcome to this template</h1>
-            <p>Now get to work and modify this app!</p>
-          </Grid.Column>
-
-        </Grid>
+        <div>
+          <Grid verticalAlign='middle' textAlign='center' container stackable>
+            <Grid.Column width={4}>
+              <Image className='' src='/images/uh-student-fat.png' size='small' centered/>
+            </Grid.Column>
+            <Grid.Column width={8}>
+              <Header as='h1'>Dorm Room Cooks</Header>
+              <p className='baloo'>Get your ono grindz right here! Find/Create incredible tasty treats designed to
+                be made from the comfort of your dorm room or apartment.</p>
+              <p className='baloo'>Recipes are crafted with nutrition, budget, and local Hawaiian vendors
+                in mind.</p>
+              <br />
+            </Grid.Column>
+          </Grid>
+        </div>
     );
   }
 }
-
 export default Landing;
