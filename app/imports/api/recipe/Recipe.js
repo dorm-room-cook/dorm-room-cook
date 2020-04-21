5e554a88e24515cb5e8f9a73ca2d1a30356db07b
joshuabh <45891115+joshuabh@users.noreply.github.com>
2020-04-20 21:05:17 -1000

    Created recipe schema
    removed nutrition field from default recipes

---
+++ app/imports/api/recipe/Recipe.js
@@ -0,0 +1,33 @@
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+import { Tracker } from 'meteor/tracker';
+
+/** Define a Mongo collection to hold the data. */
+const Recipes = new Mongo.Collection('Recipes');
+
+/** Define a schema to specify the structure of each document in the collection. */
+const RecipeSchema = new SimpleSchema({
+  title: String,
+  image: String,
+  description: String,
+  owner: String,
+  preptime: String,
+  cooktime: String,
+  cost: { type: Number, min: 0 },
+  ingredients: [String],
+  type: [String],
+  tools: { type: Array, optional: true },
+    'tools.$': String,
+  servingsize: { type: Number, min: 0 },
+  instructions: [String],
+  source: { type: String, optional: true },
+  views: Number,
+  created: String,
+  updated: String,
+}, { tracker: Tracker });
+
+/** Attach this schema to the collection. */
+Recipes.attachSchema(RecipeSchema);
+
+/** Make the collection and schema available to other code. */
+export { Recipes, RecipeSchema };
