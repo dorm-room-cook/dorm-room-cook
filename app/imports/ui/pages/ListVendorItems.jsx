1e17cbc428db95365d49841cf775ff691e66f5ff
ryanm73 <60198841+ryanm73@users.noreply.github.com>
2020-04-27 15:40:28 -1000

    +15 recipes +5 vendors

--- app/imports/ui/pages/ListVendorItems.jsx
+++ app/imports/ui/pages/ListVendorItems.jsx
@@ -4,7 +4,7 @@ import { Container, Table, Header, Loader, Button } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import { Items } from '../../api/items/Items';
-// import VendorItem from '../components/VendorItem';
+import VendorItem from '../components/VendorItem';
 
 /** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
 class ListVendorItems extends React.Component {
@@ -15,62 +15,21 @@ class ListVendorItems extends React.Component {
   }
 
   /** Render the page once subscriptions have been received. */
-  renderPage() {
-    return (
+   renderPage() {
+     return (
         <Container>
           <Header as="h2" textAlign="center">Ingredients</Header>
           <Table celled>
             <Table.Header>
               <Table.Row>
                 <Table.HeaderCell>Name</Table.HeaderCell>
-                <Table.HeaderCell>Price</Table.HeaderCell>
-                <Table.HeaderCell>Size</Table.HeaderCell>
-                <Table.HeaderCell>Vendor</Table.HeaderCell>
-                <Table.HeaderCell>Availability</Table.HeaderCell>
+                <Table.HeaderCell>Quantity</Table.HeaderCell>
+                <Table.HeaderCell>Condition</Table.HeaderCell>
                 <Table.HeaderCell>Edit</Table.HeaderCell>
               </Table.Row>
             </Table.Header>
             <Table.Body>
-              <Table.Row>
-                <Table.Cell>Chives</Table.Cell>
-                <Table.Cell>2.00</Table.Cell>
-                <Table.Cell>medium</Table.Cell>
-                <Table.Cell>Safeway</Table.Cell>
-                <Table.Cell>Available</Table.Cell>
-                <Table.Cell><Button>Edit</Button></Table.Cell>
-              </Table.Row>
-              <Table.Row>
-                <Table.Cell>Chives</Table.Cell>
-                <Table.Cell>2.50</Table.Cell>
-                <Table.Cell>medium</Table.Cell>
-                <Table.Cell>Foodland</Table.Cell>
-                <Table.Cell>Available</Table.Cell>
-                <Table.Cell><Button>Edit</Button></Table.Cell>
-              </Table.Row>
-              <Table.Row>
-                <Table.Cell>Salt</Table.Cell>
-                <Table.Cell>2.00</Table.Cell>
-                <Table.Cell>medium</Table.Cell>
-                <Table.Cell>Safeway</Table.Cell>
-                <Table.Cell>Available</Table.Cell>
-                <Table.Cell><Button>Edit</Button></Table.Cell>
-              </Table.Row>
-              <Table.Row>
-                <Table.Cell>Red Chili</Table.Cell>
-                <Table.Cell>2.00</Table.Cell>
-                <Table.Cell>medium</Table.Cell>
-                <Table.Cell>Safeway</Table.Cell>
-                <Table.Cell>Available</Table.Cell>
-                <Table.Cell><Button>Edit</Button></Table.Cell>
-              </Table.Row>
-              <Table.Row>
-                <Table.Cell>Broccoli</Table.Cell>
-                <Table.Cell>2.00</Table.Cell>
-                <Table.Cell>medium</Table.Cell>
-                <Table.Cell>Safeway</Table.Cell>
-                <Table.Cell>Available</Table.Cell>
-                <Table.Cell><Button>Edit</Button></Table.Cell>
-              </Table.Row>
+              {this.props.items.map((item) => <VendorItem key={item._id} item={item} />)}
             </Table.Body>
           </Table>
         </Container>
@@ -93,59 +52,3 @@ export default withTracker(() => {
     ready: subscription.ready(),
   };
 })(ListVendorItems);
-
-
-// import React from 'react';
-// import { Meteor } from 'meteor/meteor';
-// import { Container, Table, Header, Loader, Button } from 'semantic-ui-react';
-// import { withTracker } from 'meteor/react-meteor-data';
-// import PropTypes from 'prop-types';
-// import { Items } from '../../api/items/Items';
-// import VendorItem from '../components/VendorItem';
-//
-// /** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
-// class ListVendorItems extends React.Component {
-//
-//   /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
-//   render() {
-//     return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
-//   }
-//
-//   /** Render the page once subscriptions have been received. */
-//   renderPage() {
-//     return (
-//         <Container>
-//           <Header as="h2" textAlign="center">Ingredients</Header>
-//           <Table celled>
-//             <Table.Header>
-//               <Table.Row>
-//                 <Table.HeaderCell>Name</Table.HeaderCell>
-//                 <Table.HeaderCell>Quantity</Table.HeaderCell>
-//                 <Table.HeaderCell>Condition</Table.HeaderCell>
-//                 <Table.HeaderCell>Edit</Table.HeaderCell>
-//               </Table.Row>
-//             </Table.Header>
-//             <Table.Body>
-//               {this.props.items.map((item) => <VendorItem key={item._id} item={item} />)}
-//             </Table.Body>
-//           </Table>
-//         </Container>
-//     );
-//   }
-// }
-//
-// /** Require an array of Stuff documents in the props. */
-// ListVendorItems.propTypes = {
-//   items: PropTypes.array.isRequired,
-//   ready: PropTypes.bool.isRequired,
-// };
-//
-// /** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
-// export default withTracker(() => {
-//   // Get access to Stuff documents.
-//   const subscription = Meteor.subscribe('Items');
-//   return {
-//     items: Items.find({}).fetch(),
-//     ready: subscription.ready(),
-//   };
-// })(ListVendorItems);
