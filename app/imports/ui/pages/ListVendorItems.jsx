9a343322bb0be2f4a396f875c51d9d14d6381334
Michael Rogers <mlr2010@hawaii.edu>
2020-04-28 10:20:39 -1000

    Update ListVendorItems.jsx

--- app/imports/ui/pages/ListVendorItems.jsx
+++ app/imports/ui/pages/ListVendorItems.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Container, Table, Header, Loader, Button } from 'semantic-ui-react';
+import { Container, Table, Header, Loader } from 'semantic-ui-react';
+import swal from 'sweetalert';
+import 'uniforms-bridge-simple-schema-2'; // required for Uniforms
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
-import { Items } from '../../api/items/Items';
 import VendorItem from '../components/VendorItem';
+import { Items } from '../../api/items/Items';
 
 /** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
 class ListVendorItems extends React.Component {
@@ -14,22 +16,40 @@ class ListVendorItems extends React.Component {
     return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
   }
 
+  /** On submit, insert the data. */
+  submit(data, formRef) {
+    const { name, price, size, vendor, availability } = data;
+    const owner = Meteor.user().username;
+    Items.insert({ name, price, size, vendor, availability, owner },
+        (error) => {
+          if (error) {
+            swal('Error', error.message, 'error');
+          } else {
+            swal('Success', 'Item added successfully', 'success');
+            formRef.reset();
+          }
+        });
+  }
+
   /** Render the page once subscriptions have been received. */
-   renderPage() {
-     return (
+  renderPage() {
+    return (
         <Container>
           <Header as="h2" textAlign="center">Ingredients</Header>
           <Table celled>
             <Table.Header>
               <Table.Row>
                 <Table.HeaderCell>Name</Table.HeaderCell>
-                <Table.HeaderCell>Quantity</Table.HeaderCell>
-                <Table.HeaderCell>Condition</Table.HeaderCell>
+                <Table.HeaderCell>Price</Table.HeaderCell>
+                <Table.HeaderCell>Size</Table.HeaderCell>
+                <Table.HeaderCell>Vendor</Table.HeaderCell>
+                <Table.HeaderCell>Availability</Table.HeaderCell>
                 <Table.HeaderCell>Edit</Table.HeaderCell>
+                <Table.HeaderCell>Delete</Table.HeaderCell>
               </Table.Row>
             </Table.Header>
             <Table.Body>
-              {this.props.items.map((item) => <VendorItem key={item._id} item={item} />)}
+              {this.props.items.map((item) => <VendorItem key={item._id} item={item}/>)}
             </Table.Body>
           </Table>
         </Container>
