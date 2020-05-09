21b1951c1b3a0125186f6a821093eb8e3aa698c1
joshuabh <45891115+joshuabh@users.noreply.github.com>
2020-05-09 03:35:46 -1000

    set up for styling

--- app/imports/ui/pages/MyRecipes.jsx
+++ app/imports/ui/pages/MyRecipes.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Header, Container, Card, Loader, Grid, Modal, Button, Icon } from 'semantic-ui-react';
-import { AutoForm } from 'uniforms-semantic';
+import { ListField, ListItemField, ListAddField, NestField, AutoField } from 'uniforms-unstyled';
+import { AutoForm, TextField, LongTextField, SubmitField, ErrorsField, NumField } from 'uniforms-semantic';
+import { Header, Container, Card, Loader, Grid, Modal, Button, Icon, Segment, Form } from 'semantic-ui-react';
 import swal from 'sweetalert';
 import PropTypes from 'prop-types';
 import RecipeCard from '/imports/ui/components/RecipeCard';
@@ -18,10 +19,10 @@ class MyRecipes extends Component {
 
   /** On submit, insert the data. */
   submit(data, formRef) {
-    const { title, image, description, time, items, ingredients, type, tools, servings, instructions,
+    const { name, image, description, time, items, ingredients, type, tools, servings, instructions,
     source, views, notes, created, updated } = data;
     const owner = Meteor.user().username;
-    Recipes.insert({ title, image, description, time, items, ingredients, type, tools, servings, instructions,
+    Recipes.insert({ name, image, description, time, items, ingredients, type, tools, servings, instructions,
           source, views, notes, created, updated, owner },
         (error) => {
           if (error) {
@@ -43,7 +44,7 @@ class MyRecipes extends Component {
           <Grid>
             <Grid.Column width={16}>
               <Grid.Row text-align='centered'>
-              <Modal fluid basic closeIcon size='small'
+              <Modal basic closeIcon size='large'
                      trigger={
                        <Button icon labelPosition='left' size='small'>
                          <Icon name='add' color='red' />Add Recipe</Button>}
@@ -55,6 +56,39 @@ class MyRecipes extends Component {
                       <AutoForm ref={ref => { fRef = ref; }}
                                 schema={RecipeSchema}
                                 onSubmit={data => this.submit(data, fRef)} >
+                        <Segment>
+                          <Form.Group widths={'equal'}>
+                            <TextField name='name' showInlineError={true} placeholder='name'/>
+                            <TextField name='image' showInlineError={true} placeholder='picture URL'/>
+                            <TextField name='owner' showInlineError={true} placeholder='owner'/>
+                          </Form.Group>
+                          <LongTextField name='description' placeholder='Describe the recipe here'/>
+                          <Form.Group widths={'equal'}>
+                            <TextField name='time' showInlineError={true} placeholder='time'/>
+                            <NumField name='items' showInlineError={true} placeholder='items'/>
+                            <AutoField name="ingredients"/>
+                          </Form.Group>
+                          <Form.Group widths={'equal'}>
+                            <AutoField name="type"/>
+                            <AutoField name="tools"/>
+                            <NumField name='servings' showInlineError={true} placeholder='servings'/>
+                          </Form.Group>
+                          <Form.Group widths={'equal'}>
+                            <AutoField name="instructions"/>
+                            <TextField name='source' showInlineError={true} placeholder='source'/>
+                            <NumField name='views' showInlineError={true} placeholder='views'/>
+                          </Form.Group>
+                          <Form.Group widths={'equal'}>
+                            <NumField name='rating' showInlineError={true} placeholder='ratings'/>
+                            <AutoField name="notes"/>
+                            <TextField name='created' showInlineError={true} placeholder='created'/>
+                          </Form.Group>
+                          <Form.Group widths={'equal'}>
+                            <TextField name='updated' showInlineError={true} placeholder='updated'/>
+                          </Form.Group>
+                          <SubmitField value='Submit'/>
+                          <ErrorsField/>
+                        </Segment>
                       </AutoForm>
                     </Card.Content>
                   </Card>
